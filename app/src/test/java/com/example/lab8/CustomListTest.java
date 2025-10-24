616bc7f626008fb469307a626c5b67250215ac9d
farihaR961 <fariha5@ualberta.ca>
2025-10-24 06:59:28 +0600

    Completed Lab 8 implementation and tests

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,16 +1,23 @@
 package com.example.lab8;
 
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
-//import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
+import org.junit.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private CustomList list;
 
+    public CustomList MockCityList() {
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
 
+    @Test
+    public void addCityTest() {
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(listSize + 1, list.getCount());
+    }
 }
